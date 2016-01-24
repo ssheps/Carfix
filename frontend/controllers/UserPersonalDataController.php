67f172bbd13b115213505c0cc0065fa9da165b78
veatsly <sergey@sheps.ru>
2016-01-24 21:03:42 +0400

    Refactored AR

--- frontend/controllers/UserPersonalDataController.php
+++ frontend/controllers/UserPersonalDataController.php
@@ -8,11 +8,13 @@ class UserPersonalDataController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        $userPersonalData = new UserPersonalData();
-        $userPersonalData->user_id = 1;
-        $userPersonalData->name = 'Jury';
-        $userPersonalData->surname = 'Zalyptsov';
-        $userPersonalData->lastname = 'Anatolievitch';
+        $userPersonalData = new UserPersonalData([
+            'user_id' => 1,
+            'name' => 'Jury',
+            'surname' => 'Zalyptsov',
+            'lastname' => 'Anatolievitch'
+        ]);
+
         $userPersonalData->save();
 
         return $this->render('index', [
