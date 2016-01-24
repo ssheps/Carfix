438d42fb7eb28f2d8d714b535d335c80095aa22e
veatsly <sergey@sheps.ru>
2016-01-24 20:38:45 +0400

    Grand access for gii

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -15,7 +15,11 @@ if (!YII_ENV_TEST) {
     $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['192.168.0.*'],
+    ]
+    ;
 }
 
 return $config;
